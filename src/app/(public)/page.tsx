f07052fa00eccd5dba63094fe63cc9fe61980c84
Hitalo Diniz <hitalodiniz@gmail.com>
2026-01-07 15:14:55 -0300

    Ajuste de layout das páginas publicas

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 };
 
 export default async function HomePage() {
-  //return <LandingPageContent />;
-  return <LoadingScreen message="Carregando..." />;
+  return <LandingPageContent />;
+  // return <LoadingScreen message="Carregando..." />;
   //return <GlobalError />;
 }
